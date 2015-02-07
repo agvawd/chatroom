f18bec73723b98f350e1b8eedd412e2b2f5a6c1f
Allen <agvawd@hotmail.com>
2015-02-06 18:16:57 -0700

    Styled it up and added a time stamp of when the message was created

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,6 +1,36 @@
 var app = angular.module('chatroom');
 
+
 app.controller('mainCtrl', function($scope, parseService){
+  $scope.timestamp = function timeStamp() {
+// Create a date object with the current time
+  var now = new Date();
+ 
+// Create an array with the current month, day and time
+  var date = [ now.getMonth() + 1, now.getDate(), now.getFullYear() ];
+ 
+// Create an array with the current hour, minute and second
+  var time = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
+ 
+// Determine AM or PM suffix based on the hour
+  var suffix = ( time[0] < 12 ) ? "AM" : "PM";
+ 
+// Convert hour from military time
+  time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
+ 
+// If hour is 0, set it to 12
+  time[0] = time[0] || 12;
+ 
+// If seconds and minutes are less than 10, add a zero
+  for ( var i = 1; i < 3; i++ ) {
+    if ( time[i] < 10 ) {
+      time[i] = "0" + time[i];
+    }
+  }
+ 
+// Return the formatted string
+  return date.join("/") + " " + time.join(":") + " " + suffix;
+}
   //In your controller you'll have a getParseData function and a postData function, but should be placed on $scope.
   $scope.messages = [];
   //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to 
@@ -20,8 +50,12 @@ app.controller('mainCtrl', function($scope, parseService){
     parseService.postData($scope.message).then(function(data){
       data = $scope.message;
     });
+    $scope.message = '';
   }
 
+  $scope.formatDate = function(dateStr) {
+    return new Date(dateStr).toLocaleString();
+  }
 
 
   //uncomment this code when your getParseData function is finished
